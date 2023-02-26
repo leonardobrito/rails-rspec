bbbaf54b7ecaa19c73fbd82019656fd197fa35ec
leonardobrito <f.leobrito@gmail.com>
2023-02-26 12:31:18 -0300

    :passport_control: VCR cassette record

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -2,13 +2,16 @@
 
 describe HTTParty do
   it 'content-type' do
-    stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2')
-      .to_return(status: 200, body: '', headers: {
-                   'content-type': 'application/json'
-                 })
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    # stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2')
+    #   .to_return(status: 200, body: '', headers: {
+    #                'content-type': 'application/json'
+    #              })
 
-    content_type = response.headers['content-type']
-    expect(content_type).to match(%r{application/json})
+    VCR.use_cassette('jsonplaceholder/posts') do
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+
+      content_type = response.headers['content-type']
+      expect(content_type).to match(%r{application/json})
+    end
   end
 end
