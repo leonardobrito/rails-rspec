aebe83f0df359f88decdda11df8cbab65e8990dd
leonardobrito <f.leobrito@gmail.com>
2023-02-26 01:39:15 -0300

    :bento: Inheritance

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -21,10 +21,20 @@ RSpec.describe Customer, type: :model do
   end
 
   it '#full_name with Sr. Leonardo' do
-    # override attributes
+    # override attributes and aliases [:user]
     customer = create(:user, name: 'Leonardo')
 
     expect(customer.full_name).to eq('Sr. Leonardo')
+  end
+
+  it '#vip' do
+    # override attributes
+    # customer = create(:customer, vip: false, days_to_pay: 15)
+    customer = create(:customer_vip)
+    customer_default = create(:customer_default)
+
+    expect(customer.vip).to be_truthy
+    expect(customer_default.vip).to be_falsey
   end
 
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
