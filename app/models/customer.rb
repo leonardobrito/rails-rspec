2c6ea99c7356338629678090e23c7ef41d1517dc
leonardobrito <f.leobrito@gmail.com>
2023-02-26 01:13:58 -0300

    :clown_face: Fixtures

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# Customer
 class Customer < ApplicationRecord
+  def full_name
+    "Sr. #{name}"
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # fixtures :all
+  fixtures :customers
+
+  it 'creates a customer' do
+    # subject.name = 'Leonardo'
+    # subject.email = 'f.leobrito@gmail.com'
+    customer = customers(:leonardo)
+    other_customer = customers(:john)
+
+    expect(customer.full_name).to eq('Sr. Leonardo Brito')
+    expect(other_customer.full_name).to eq('Sr. John Doe')
+  end
 end
