78044fc62ee86f85618d86d9f2dc99bfcd79852a
leonardobrito <f.leobrito@gmail.com>
2023-02-26 18:33:15 -0300

    :art: Page object pattern

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'support/new_customer_form'
 
 RSpec.feature 'Customers', type: :feature, js: true do
   context '#index' do
@@ -37,6 +38,18 @@ RSpec.feature 'Customers', type: :feature, js: true do
       fill_in('Email', with: Faker::Internet.email)
       fill_in('Address', with: Faker::Address.street_address)
       click_button('Create Customer')
+
+      expect(page).to have_content('Customer was successfully created.')
+    end
+
+    it 'creates a new customer with page object pattern' do
+      new_customer_form = NewCustomerForm.new
+
+      new_customer_form.login.visit_page.fill_in_with(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        address: Faker::Address.street_address
+      ).submit
 
       expect(page).to have_content('Customer was successfully created.')
     end
