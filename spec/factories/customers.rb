e87176968bc1c95059c946a3059cb27841ad4bda
leonardobrito <f.leobrito@gmail.com>
2023-02-26 01:17:11 -0300

    :technologist: Factory bot

---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :customer do
+    name {'Leonardo Brito'}
+    email {'f.leonardo@gmail.com'}
+  end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,15 +4,18 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   # fixtures :all
-  fixtures :customers
+  # fixtures :customers
 
   it 'creates a customer' do
     # subject.name = 'Leonardo'
     # subject.email = 'f.leobrito@gmail.com'
-    customer = customers(:leonardo)
-    other_customer = customers(:john)
+    # customer = customers(:leonardo)
+    # other_customer = customers(:john)
+
+    # customer = build(:customer)
+    customer = create(:customer)
 
     expect(customer.full_name).to eq('Sr. Leonardo Brito')
-    expect(other_customer.full_name).to eq('Sr. John Doe')
+    # expect(other_customer.full_name).to eq('Sr. John Doe')
   end
 end
